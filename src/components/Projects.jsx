03265d1f287cfbf24b3607a27928d0c4c8f75ba6
Alexander Novo <91619180+alexandernovo@users.noreply.github.com>
2024-04-26 23:43:47 +0800

    credential updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,8 +14,8 @@ const Projects = () => {
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-[16px] font-bold tracking-tight text-gray-900 dark:text-white">Novel System</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Created a Novel Sytem for user to read novels with awesome UI and Interface.</p>
-                        <p className='text-white text-[13px]'>Username: admin@gmail.com</p>
-                        <p className='text-white text-[13px]'>Password: 123</p>
+                        <p className='text-white text-[9px]'>Username: admin@gmail.com</p>
+                        <p className='text-white text-[9px]'>Password: 123</p>
                     </div>
                 </a>
                 <a target='_blank' href="http://alexander-novo.free.nf/ROTC" class="overflow-hidden pl-5 flex w-[50%] flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" >
@@ -23,8 +23,8 @@ const Projects = () => {
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-[16px] font-bold tracking-tight text-gray-900 dark:text-white">ROTC Attendance and Merit System</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">ROTC Attendance and Merit System is used to manage grades by recording attendance and merits of the cadets.</p>
-                        <p className='text-white text-[13px]'>Username: Admin</p>
-                        <p className='text-white text-[13px]'>Password: Admin</p>
+                        <p className='text-white text-[9px]'>Username: Admin</p>
+                        <p className='text-white text-[9px]'>Password: Admin</p>
                     </div>
                 </a>
             </div>
@@ -34,8 +34,8 @@ const Projects = () => {
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-[16px] font-bold tracking-tight text-gray-900 dark:text-white">Dental Clinic System</h5>
                         <p class="font-normal text-gray-700 dark:text-gray-400">Dental Clinic System is used to manage dental health care and record what tooth/teeths have been filled, extract etc.</p>
-                        <p className='text-white text-[13px]'>Username: Phyllis@gmail.com</p>
-                        <p className='text-white text-[13px]'>Password: Phyllis@gmail.com</p>
+                        <p className='text-white text-[9px]'>Username: Phyllis@gmail.com</p>
+                        <p className='text-white text-[9px]'>Password: Phyllis@gmail.com</p>
                     </div>
                 </a>
             </div>
